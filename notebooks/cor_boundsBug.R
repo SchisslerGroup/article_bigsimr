b18fb36669583b8e20e44219dd14da6d7b552aae
Alex Knudson <aknudson@nevada.unr.edu>
2021-02-27 17:19:06 -0800

    re-add notebooks so Grant can check out new branch without losing files

---
+++ notebooks/cor_boundsBug.R
@@ -0,0 +1,12 @@
+library(bigsimr)
+size <- 4
+prob <- 3e-04
+type <- "spearman"
+n <- 1e3
+
+margins <- alist(
+        qnbinom(size = size, prob = prob),
+        qnbinom(size = size, prob = prob)
+)
+
+tmp_bounds <- cor_bounds(margins, method = type)
